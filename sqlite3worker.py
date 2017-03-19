71bed0c28a2eafcbfa818ecd07f3d69682eaec89
Shawn Lee <dashawn@gmail.com>
2017-03-19 13:15:42 -0400

    Wrong order

--- sqlite3worker.py
+++ sqlite3worker.py
@@ -1,5 +1,5 @@
-# Copyright (c) 2014 Palantir Technologies
 # -*- coding: utf-8 -*-
+# Copyright (c) 2014 Palantir Technologies
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
