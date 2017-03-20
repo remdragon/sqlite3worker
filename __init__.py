f73189fc1732779278f03b5269039a1d1642d5fc
Shawn Lee <dashawn@gmail.com>
2017-03-20 07:45:50 -0700

    Python2 vs Python3 compat.

--- __init__.py
+++ __init__.py
@@ -25,4 +25,9 @@ __email__ = "dashawn@gmail.com"
 __license__ = "MIT"
 __version__ = "1.1.5"
 
-from sqlite3worker.sqlite3worker import Sqlite3Worker
+try:
+    # Python 2
+    from sqlite3worker import Sqlite3Worker
+except ImportError:
+    # Python 3
+    from sqlite3worker.sqlite3worker import Sqlite3Worker
